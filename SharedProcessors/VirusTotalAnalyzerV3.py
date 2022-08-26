2cd20fb4be12e667f3441e1eb84f3e8c43db28d8
Ian Cohn <icc2@psu.edu>
2022-08-26 12:30:02 -0400

    WIP: VirusTotalAnalyzerV3

--- SharedProcessors/VirusTotalAnalyzerV3.py
+++ SharedProcessors/VirusTotalAnalyzerV3.py
@@ -150,7 +150,7 @@ class VirusTotalAnalyzerV3(URLDownloader):
 
 		# If not exists or last scan is too old
 			if len(jsonResponse["data"]) != 1 or \
-			((int(jsonResponse["data"]["attributes"]["date"]) < minTimeEpoch) == False):
+			((int(jsonResponse["data"]["attributes"]["last_analysis_date"]) < minTimeEpoch) == False):
 			# Submit File
 				self.output("File does not exist in Virus Total, or there are ambiguous results. Beginning upload process now.", verbose_level=1)
 				fileSize = int(path.getsize(filePath))
