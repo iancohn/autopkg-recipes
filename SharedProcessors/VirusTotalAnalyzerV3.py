2a52058e783999feea4e56a0f8bbdb559f49976f
Ian Cohn <icc2@psu.edu>
2022-09-01 07:55:49 -0400

    Account for unsigned objects

--- SharedProcessors/VirusTotalAnalyzerV3.py
+++ SharedProcessors/VirusTotalAnalyzerV3.py
@@ -357,6 +357,7 @@ class VirusTotalAnalyzerV3(URLDownloader):
 			if len(codeSignConfig["code_signing_checks"]) > 0:
 				continueOnVerificationFailure:bool = codeSignConfig.get("continue_on_failure") or False
 				self.output("Validating provided {} provided code signing checks.".format(len(codeSignConfig["code_signing_checks"])), verbose_level=2)
+				self.output("Code signing checks: {}".format(codeSignConfig["code_signing_checks"]), verbose_level=3)
 				checks = list(codeSignConfig["code_signing_checks"].keys())
 				codeSignVerificationFailed = False
 				for check in checks:
